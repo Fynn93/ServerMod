7db233b2e8015228a196339eea0277630c33df8d
lumentae <66220444+lumentae@users.noreply.github.com>
2024-11-10 00:17:23 +0100

    Complete support for messaging from/to discord, timestamp when sending message and german language

--- src/main/java/de/fynn93/servermod/Config.java
+++ src/main/java/de/fynn93/servermod/Config.java
@@ -1,6 +1,8 @@
 package de.fynn93.servermod;
 
 public class Config {
+    public String token = "";
+    public String channelId = "";
     public String webhookUrl = "";
     public String joinPingRoleId = "";
 }
